efad0c494262059fcf8608cb975548f738bbcf9a
tamaraglisic <39412716+tamaraglisic@users.noreply.github.com>
2022-09-14 19:10:06 +0200

    test: integration tests for account, company, comment, rate services

---
+++ agentapi/src/test/java/com/dislinkt/agentapi/constants/CompanyConstants.java
@@ -0,0 +1,29 @@
+package com.dislinkt.agentapi.constants;
+
+public class CompanyConstants {
+
+	
+	public static final Long DB_COMPANY_ID = 1L;
+	public static final String DB_COMPANY_UUID = "1c20fb12-60d8-4322-ba33-9c05203868e9";
+	public static final String DB_COMPANY_NAME = "SpaceX";
+	public static final String DB_COOMPANY_ADDRESS = "California";
+	
+	public static final String DB_REQUEST_NAME = "Fun travel";
+	public static final String DB_REQUEST_STATUS = "PENDING";
+	public static final String DB_REQUEST_ADDRESS = "Belgrade";
+	public static final String DB_REQUEST_PHONE = "0123";
+	public static final String DB_REQUEST_DESC = "travel agency";
+	public static final String DB_REQUEST_UUID = "1c20fb12-60d8-4322-ba53-9c05203868e9";
+	public static final String DB_REQUEST_OWNER_UUID = "7c20fb12-60d8-4322-ba33-9c05203868e9";
+	
+	public static final String NEW_REQUEST_NAME = "New comapny";
+	public static final String NEW_REQUEST_STATUS = "PENDING";
+	public static final String NEW_REQUEST_ADDRESS = "Belgrade";
+	public static final String NEW_REQUEST_PHONE = "0123";
+	public static final String NEW_REQUEST_DESC = "the best company";
+	
+	public static final Integer PAGEABLE_PAGE = 0;
+    public static final Integer PAGEABLE_SIZE = 5;
+    
+    public static final String COMPANY_UUID_DoesntExist = "123";
+}
