e5c46bb57dd0e36d9425c8c4a93a81a8bc7c5fa6
Petar Cerović <quadrition@gmail.com>
2022-09-20 02:30:32 +0200

    feat: add company

--- agentapi/src/main/java/com/dislinkt/agentapi/web/rest/company/CompanyResource.java
+++ agentapi/src/main/java/com/dislinkt/agentapi/web/rest/company/CompanyResource.java
@@ -1,11 +1,10 @@
 package com.dislinkt.agentapi.web.rest.company;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.dislinkt.agentapi.service.company.CompanyService;
 import com.dislinkt.agentapi.util.ReturnResponse;
@@ -19,6 +18,10 @@ public class CompanyResource {
 	@Autowired
 	private CompanyService companyService;
 
+    @GetMapping
+    private ResponseEntity<Page<CompanyDTO>> findAll(Pageable pageable) {
+        return ReturnResponse.entityGet(companyService.findAll(pageable));
+    }
 
     @PostMapping
     private ResponseEntity<CompanyDTO> insertCompany(@RequestBody CompanyRequestDTO companyRequest) {
